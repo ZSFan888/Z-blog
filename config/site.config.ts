2dacd7b64788d6e8c0304fef7c907feb6adc4751
ZSFan <zsfan-nb@hotmail.com>
2026-06-14 02:41:20 +0000

    重置为全新博客

--- config/site.config.ts
+++ config/site.config.ts
@@ -1,12 +1,12 @@
 // 博客配置文件
 // VITE_SITE_URL 环境变量优先于此处的 url 配置
 export const siteConfig = {
-  title: 'ZSFan 的博客',
-  subtitle: '凡的博客',
-  description: 'ZSFan 的个人博客，记录技术与生活',
+  title: 'New Blog',
+  subtitle: '一个全新的博客',
+  description: '一个重新开始的个人博客，内容将从这里重新整理与发布。',
   logo: '/logo.png',
   seoImage: '/logo.png',
-  footerText: '©2026 ZSFan 的 Blog',
+  footerText: '© 2026 New Blog',
   url: 'https://blog-buh.pages.dev',
   social: {
     github: 'https://github.com/ZSFan888/',
@@ -14,10 +14,10 @@ export const siteConfig = {
     rawEmail: 'zsfan-nb@hotmail.com',
   },
   author: {
-    name: 'ZSFan',
+    name: 'New Blog',
     avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1453508737&s=100',
-    role: '开发者',
-    bio: '你好！我是 ZSFan，欢迎来到我的博客。这里记录我的技术探索与生活随笔。',
+    role: 'Blogger',
+    bio: '这里是一个全新的开始。',
   },
   toc: {
     collapseInactiveRootBranches: true,
@@ -26,7 +26,7 @@ export const siteConfig = {
     repoUrl: 'https://github.com/ZSFan888/Blog',
     repoFriendsUrl: 'https://github.com/ZSFan888/Blog/tree/main/friends',
     repoFriendsDir: 'friends',
-    announcement: '友链申请请前往 GitHub 仓库，在 friends 目录下参考已有 JSON 格式新增文件并提交 PR。提交前请确认已添加本站友链。',
+    announcement: '博客内容已重置，新的内容正在整理中。',
   },
   beian: {
     text: '',
